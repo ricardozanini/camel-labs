75062cd7c25f56865150c0c11f8b0b8784c9ad80
Ricardo Zanini <zanini@redhat.com>
2017-11-22 13:03:06 -0200

    initial commit

---
+++ src/test/java/com/stackoverflow/camel/labs/JettyServeStaticRouterTest.java
@@ -0,0 +1,108 @@
+package com.stackoverflow.camel.labs;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.DisableJmx;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+//https://stackoverflow.com/questions/47406799/serving-static-files-with-camel-routes
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@DisableJmx(true)
+public class JettyServeStaticRouterTest extends CamelTestSupport {
+
+    private static final String URL = "http://0.0.0.0:8080/user/dist";
+    private static final String ENDPOINT = "jetty:" + URL + "?matchOnUriPrefix=true";
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        return camelContext;
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+
+                from(ENDPOINT).process(new Processor() {
+
+                    public void process(Exchange exchange) throws Exception {
+                        Message in = exchange.getIn();
+
+                        String relativepath = in.getHeader(Exchange.HTTP_PATH, String.class);
+                        String requestPath = in.getHeader("CamelServletContextPath", String.class); //CamelServletContextPath
+
+                        if (relativepath.isEmpty() || relativepath.equals("/")) {
+                            relativepath = "index.html";
+                        }
+
+                        final String formattedPath = String.format("%s/%s", requestPath, relativepath);
+                        InputStream pathStream = this.getClass().getResourceAsStream(formattedPath);
+                        Path path = FileSystems.getDefault().getPath(this.getClass().getResource(formattedPath).getPath());
+
+                        Message out = exchange.getOut();
+                        try {
+                            out.setBody(IOUtils.toByteArray(pathStream));
+                            out.setHeader(Exchange.CONTENT_TYPE, Files.probeContentType(path));
+                        } catch (IOException e) {
+                            out.setBody(relativepath + " not found.");
+                            out.setHeader(Exchange.HTTP_RESPONSE_CODE, "404");
+                        }
+                    }
+                }).routeId("static");
+
+            }
+        };
+    }
+
+    @Test
+    public void testMessage() throws InterruptedException {
+        final String response = template.requestBody(ENDPOINT, "", String.class);
+        assertNotNull(response);
+        assertThat(response, containsString("Hello"));
+        //Thread.sleep(100000); //small pause to test in browser.
+    }
+
+    @Test
+    public void testRequest() throws ClientProtocolException, IOException {
+        final Response response = Request.Get(URL  + "index.html").execute();
+        assertThat(response.returnContent().asString(), containsString("Hello"));
+    }
+
+    @Test
+    public void testRequestBrowserReload() throws ClientProtocolException, IOException {
+        final Response response = Request.Get(URL).execute();
+        assertThat(response.returnContent().asString(), containsString("Hello"));
+    }
+
+}
---
+++ src/test/java/com/stackoverflow/camel/labs/Netty4RouterTest.java
@@ -0,0 +1,74 @@
+package com.stackoverflow.camel.labs;
+
+import static org.hamcrest.Matchers.is;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.DisableJmx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@DisableJmx(true)
+public class Netty4RouterTest extends CamelTestSupport {
+
+    private static final String ENDPOINT = "netty4:tcp://localhost:5150?textline=true&delimiter=NULL";
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        return camelContext;
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from(ENDPOINT).routeId("nett4-route").log("Body is: " + body()).process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        final String body = exchange.getIn().getBody(String.class);
+                        final String response = String.format("%s,%s,%s", body, body.length(), body.getBytes());
+                        exchange.getOut().setBody(response);
+                    }
+                });
+            }
+        };
+    }
+
+    @Test
+    public void test() throws JsonParseException, JsonMappingException, IOException {
+        assertNotNull(camelContext);
+        final String body = "hello world!" + System.getProperty("line.separator");
+        final Map<String, Object> request = new HashMap<String, Object>();
+        request.put("message", body);
+        request.put("len", body.length());
+        final String responseBody = template.requestBody(ENDPOINT, body, String.class);
+        assertNotNull(responseBody);
+        final String[] responseArr = responseBody.split(",");
+
+        assertThat(responseArr[0], is(request.get("message")));
+        assertThat(Integer.parseInt(responseArr[1]), is((Integer)request.get("len")));
+    }
+
+}
