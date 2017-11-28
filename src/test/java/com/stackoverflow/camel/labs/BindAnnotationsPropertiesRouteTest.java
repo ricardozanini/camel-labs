33591781fbe57826945fc97e9abc25d8f2356e62
Ricardo Zanini <zanini@redhat.com>
2017-11-27 23:42:48 -0200

    questions 47517933 and 47515393

---
+++ src/test/java/com/stackoverflow/camel/labs/BindAnnotationsPropertiesRouteTest.java
@@ -0,0 +1,73 @@
+package com.stackoverflow.camel.labs;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Properties;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.DisableJmx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest()
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@DisableJmx(true)
+// https://stackoverflow.com/questions/47515393/using-parameter-bind-anotations-properties-is-null
+public class BindAnnotationsPropertiesRouteTest extends CamelTestSupport {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BindAnnotationsPropertiesRouteTest.class);
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        return camelContext;
+    }
+    
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    .setProperty("myProp", constant("Hello World"))
+                    .bean(new BeanBindProps(), "methodA('serviceName')")
+                    .log("the body is +" + body())
+                    .to("mock:result");
+            }
+        };
+    }
+
+    public static class BeanBindProps {
+        public BeanBindProps() {
+            LOGGER.info("new class");
+        }
+
+        public void methodA(String eventName, @Properties Map<String, Object> properties) {
+            LOGGER.info("The properties are: {}", properties);
+            LOGGER.info("The event name is: {}", eventName);
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        // with mocks nothing works
+        getMockEndpoint("mock:result").expectedMessageCount(1);
+        getMockEndpoint("mock:result").expectedPropertyReceived("myProp", "Hello World");
+        template.sendBody("direct:start", "myBody");
+        assertMockEndpointsSatisfied(10, TimeUnit.SECONDS);
+    }
+}
---
+++ src/test/java/com/stackoverflow/camel/labs/Netty4RetrieveStatusCode.java
@@ -0,0 +1,71 @@
+package com.stackoverflow.camel.labs;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.common.HttpMethods;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.DisableJmx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@DisableJmx(true)
+//https://stackoverflow.com/questions/47517933/jboss-fuse-camel-dsl-using-camel-netty4-http-how-to-i-obtain-the-rest-return-c
+public class Netty4RetrieveStatusCode extends CamelTestSupport {
+
+    private static final String ENDPOINT = "netty4:tcp://localhost:5150?textline=true&delimiter=NULL";
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        return camelContext;
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+                    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                    .to("netty4-http:http://localhost:8080")
+                    .log("The response code is: ${header["+Exchange.HTTP_RESPONSE_CODE+"]}")
+                    .bean(new Processor() {
+                        public void process(Exchange exchange) throws Exception {
+                            final Integer code = (Integer)exchange.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE);
+                            exchange.getIn().setHeader("responseCode", code);
+                        }
+                    });
+
+                from("jetty:http://0.0.0.0:8080").process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        exchange.getOut().setBody("OK");
+                    }
+                });
+            }
+        };
+    }
+
+    @Test
+    public void test() {
+        final String body = "{say : 'hello'}";
+        final Exchange response = template.send("direct:start", createExchangeWithBody(body));
+        assertThat((Integer)response.getIn().getHeader("responseCode"), is(200));
+    }
+
+}
