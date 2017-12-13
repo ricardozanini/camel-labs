bd03e0659bd36725d988fad8fa520a9092c98f85
Ricardo Zanini <zanini@redhat.com>
2017-12-13 15:01:40 -0200

    question 47782520 update

--- src/test/java/com/stackoverflow/camel/labs/PollEnrichDynamicEndpointRouterTest.java
+++ src/test/java/com/stackoverflow/camel/labs/PollEnrichDynamicEndpointRouterTest.java
@@ -2,18 +2,27 @@ package com.stackoverflow.camel.labs;
 
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.apache.camel.test.spring.DisableJmx;
+import org.hamcrest.collection.IsArrayContaining;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -23,6 +32,16 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 @DisableJmx(true)
 // https://stackoverflow.com/questions/47782520/camel-enrich-sql-syntax-issue
 public class PollEnrichDynamicEndpointRouterTest extends CamelTestSupport {
+    
+    EmbeddedDatabase db;
+
+    @Before
+    public void setUp() throws Exception {
+        db = new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.DERBY).addScript("sql/createAndPopulateDatabase.sql").build();
+
+        super.setUp();
+    }
     
     @Autowired
     private CamelContext camelContext;
@@ -35,8 +54,11 @@ public class PollEnrichDynamicEndpointRouterTest extends CamelTestSupport {
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
+                       
             @Override
             public void configure() throws Exception {
+                getContext().getComponent("sql", SqlComponent.class).setDataSource(db);
+                
                 from("direct:start")
                     .setHeader("userID", constant(1001))
                     .enrich("mock:direct:enrich${in.header.userID}", new AggregationStrategy() {
@@ -48,6 +70,17 @@ public class PollEnrichDynamicEndpointRouterTest extends CamelTestSupport {
                     })
                     .log("the body ${body}")
                     .to("mock:result");
+                
+                from("direct:sql_start")
+                    .setHeader("userID", constant(1))
+                    .enrich("sql:select project from projects where id = :#${in.header.userID}", new AggregationStrategy() {
+                        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+                            //nothing
+                            return newExchange;
+                        }
+                    })
+                    .log("the body ${body}")
+                    .to("mock:result_sql");
             }
         };
     }
@@ -57,6 +90,16 @@ public class PollEnrichDynamicEndpointRouterTest extends CamelTestSupport {
         getMockEndpoint("mock:result").expectedMessageCount(1);
         final Object body = template.requestBody("direct:start", "test");
         assertThat((Integer)body, is(1001));
+        assertMockEndpointsSatisfied();
+    }
+    
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Test
+    public void test_sql() throws InterruptedException {
+        getMockEndpoint("mock:result_sql").expectedMessageCount(1);
+        final ArrayList<Map<String, Object>> body = (ArrayList)template.requestBody("direct:sql_start", "test");
+        assertNotNull(body);
+        assertThat(body.get(0).get("PROJECT").toString(), is("Camel"));
         assertMockEndpointsSatisfied();
     }
 
